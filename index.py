0b555ddf9aecb51bbd3749cb5fca3b57512b4bfc
Viko Wijaya <vwijaya08@gmail.com>
2024-06-25 12:35:17 +0700

    second task done

--- index.py
+++ index.py
@@ -11,3 +11,6 @@ schools.head()
 # Add as many cells as you like...
 filter_best_math_80_percent = schools[schools['average_math'] >= (800 * (80 / 100))]
 best_math_schools = filter_best_math_80_percent[['school_name', 'average_math']].sort_values('average_math', ascending=False)
+
+schools['total_SAT'] = schools['average_math'] + schools['average_reading'] + schools['average_writing']
+top_10_schools = schools[['school_name', 'total_SAT']].sort_values('total_SAT', ascending=False).head(10)
